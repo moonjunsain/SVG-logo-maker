2ccae826eb610560a563d37f55c6ce0343fb20b1
moonjunsain <moonjunsain@gmail.com>
2023-07-25 18:09:18 -0700

    deleted unnecessary html file

--- index.js
+++ index.js
@@ -78,9 +78,9 @@ function renderLogo({text, textColor, shape, shapeColor}){
 // function for generating svg
 function generateSVG(logo){
     return `
-    <svg version="1.1"
-     width="300" height="200"
-     xmlns="http://www.w3.org/2000/svg">
+<svg version="1.1"
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg">
 
   ${logo.render()}
 
